8c610142bff371f5b1a20bd2a929b5b6a9d77885
Dhanush <dhanush@Dhanushs-MacBook-Pro.local>
2025-12-24 14:10:50 +0530

    fix: reverse the y axis

--- sdk_overrides/kernel/drivers/input/touchscreen/gt9xx/gt9xx.c
+++ sdk_overrides/kernel/drivers/input/touchscreen/gt9xx/gt9xx.c
@@ -2655,7 +2655,7 @@ static int goodix_ts_probe(struct i2c_client *client, const struct i2c_device_id
 		m89or101 = TRUE;
 		gtp_change_x2y = FALSE;
 		gtp_x_reverse = FALSE;
-		gtp_y_reverse = FALSE;
+		gtp_y_reverse = TRUE;
 	} else if (val == 101) {
 		m89or101 = FALSE;
 		gtp_change_x2y = TRUE;
