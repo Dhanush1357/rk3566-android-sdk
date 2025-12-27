c5c4578bbed666a32aea6bdb3b2015eebf098033
atom.ai <contact@atom.ai>
2025-12-27 10:32:51 +0530

    Revert "fix: x mirror value change"
    This reverts commit e066b2ad7ac29ebd2c84ef3fccb00e676a4a6bbc.

--- sdk_overrides/kernel/drivers/input/touchscreen/gt9xx/gt9xx.c
+++ sdk_overrides/kernel/drivers/input/touchscreen/gt9xx/gt9xx.c
@@ -432,8 +432,7 @@ static void gtp_touch_down(struct goodix_ts_data* ts,s32 id,s32 x,s32 y,s32 w)
 		if (gtp_y_reverse)
 			y = ts->abs_y_max - y;
 	}
-
-    /* ===== GT911 final orientation fix ===== */
+/* ===== GT911 touch orientation fix (final) ===== */
 {
     int tx = x;
     int ty = y;
@@ -442,11 +441,10 @@ static void gtp_touch_down(struct goodix_ts_data* ts,s32 id,s32 x,s32 y,s32 w)
     x = ty;
     y = ts->abs_x_max - tx;
 
-    /* FIX: mirror X axis */
+    /* FIX: invert X axis */
     x = ts->abs_y_max - x;
 }
-/* ====================================== */
-
+/* ============================================== */
 
 
 
