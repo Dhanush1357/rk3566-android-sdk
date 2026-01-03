f981c4033b2596fe478f6d8bf73aa3a8d6eba555
atom.ai <contact@atom.ai>
2026-01-03 19:58:08 +0530

    fix: x reverse enabele in else condition

--- sdk_overrides/kernel/drivers/input/touchscreen/gt9xx/gt9xx.h
+++ sdk_overrides/kernel/drivers/input/touchscreen/gt9xx/gt9xx.h
@@ -50,8 +50,8 @@
 #define GTP_Y_REVERSE_ENABLE	1
 #else
 #define GTP_CHANGE_X2Y        1
-#define GTP_X_REVERSE_ENABLE	1
-#define GTP_Y_REVERSE_ENABLE	0
+#define GTP_X_REVERSE_ENABLE	0
+#define GTP_Y_REVERSE_ENABLE	1
 #endif
 #define GTP_DRIVER_SEND_CFG   1
 #define GTP_HAVE_TOUCH_KEY    0
