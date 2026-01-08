0392d2ad8bb6fa2a37f5ba4bf989e6b8e61bf6ba
atom.ai <contact@atom.ai>
2026-01-08 10:06:46 +0530

    fix: driver send config enable v5

--- sdk_overrides/kernel/drivers/input/touchscreen/gt9xx/gt9xx.h
+++ sdk_overrides/kernel/drivers/input/touchscreen/gt9xx/gt9xx.h
@@ -46,7 +46,7 @@
 #define GTP_CUSTOM_CFG        0
 #if CONFIG_8_9
 #define GTP_CHANGE_X2Y        0
-#define GTP_X_REVERSE_ENABLE	0
+#define GTP_X_REVERSE_ENABLE	1
 #define GTP_Y_REVERSE_ENABLE	1
 #else
 #define GTP_CHANGE_X2Y        1
