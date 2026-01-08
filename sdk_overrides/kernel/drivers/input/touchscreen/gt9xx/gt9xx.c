328880ae5094417503ae9b933e097802ff8b81f9
atom.ai <contact@atom.ai>
2026-01-08 11:26:51 +0530

    fix: driver send config enable v9

--- sdk_overrides/kernel/drivers/input/touchscreen/gt9xx/gt9xx.c
+++ sdk_overrides/kernel/drivers/input/touchscreen/gt9xx/gt9xx.c
@@ -422,15 +422,13 @@ Output:
 *********************************************************/
 static void gtp_touch_down(struct goodix_ts_data* ts,s32 id,s32 x,s32 y,s32 w)
 {
-	if (gtp_change_x2y)
-		GTP_SWAP(x, y);
+	/* Apply reverse logic for GT911 as well */
+if (gtp_x_reverse)
+    x = ts->abs_x_max - x;
 
-	if (!bgt911 && !bgt970) {
-		if (gtp_x_reverse)
-			x = ts->abs_x_max - x;
+if (gtp_y_reverse)
+    y = ts->abs_y_max - y;
 
-		if (gtp_y_reverse)
-			y = ts->abs_y_max - y;
 	}
 
 #if GTP_ICS_SLOT_REPORT
