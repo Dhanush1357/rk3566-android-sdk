2f1f0e762619aeb0a7fef98abfd7069a41942a6e
atom.ai <contact@atom.ai>
2026-01-21 16:06:28 +0530

    fix: second touch screen coordination chnages v2

--- sdk_overrides/kernel/drivers/input/touchscreen/gt9xx/gt9xx.c
+++ sdk_overrides/kernel/drivers/input/touchscreen/gt9xx/gt9xx.c
@@ -421,9 +421,9 @@ Output:
 static void gtp_touch_down(struct goodix_ts_data* ts,s32 id,s32 x,s32 y,s32 w)
 {
 	if (bgt911) {
-        
+
     /* mirror X to match LCD orientation */
-    x = ts->abs_x_max - x;
+    x = ts->abs_x_max -1 - x;
 }
 
 
