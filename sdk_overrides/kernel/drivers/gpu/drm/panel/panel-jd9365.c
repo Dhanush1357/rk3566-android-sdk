26c355e18c68e88c007e5c8dd7e57b15c8493a87
atom.ai <contact@atom.ai>
2026-02-05 13:48:58 +0530

    fix: disable the dsi route in dtsi v10

--- sdk_overrides/kernel/drivers/gpu/drm/panel/panel-jd9365.c
+++ sdk_overrides/kernel/drivers/gpu/drm/panel/panel-jd9365.c
@@ -219,14 +219,13 @@ static int jd9365_probe(struct mipi_dsi_device *dsi)
 	dsi->lanes = 4;
 	dsi->format = MIPI_DSI_FMT_RGB888;
 	dsi->mode_flags = MIPI_DSI_MODE_VIDEO |
-		  MIPI_DSI_MODE_VIDEO_BURST |
-		  MIPI_DSI_MODE_LPM;
+			  MIPI_DSI_MODE_VIDEO_BURST |
+			  MIPI_DSI_MODE_LPM;
 
-	/* OLD kernels (RK3566 Android) use this style */
+	/* OLD DRM API (Rockchip Android) */
 	drm_panel_init(&ctx->panel);
 	ctx->panel.dev = &dsi->dev;
 	ctx->panel.funcs = &jd9365_panel_funcs;
-	ctx->panel.connector_type = DRM_MODE_CONNECTOR_DSI;
 
 	drm_panel_add(&ctx->panel);
 
@@ -245,8 +244,6 @@ static void jd9365_remove(struct mipi_dsi_device *dsi)
 
 	mipi_dsi_detach(dsi);
 	drm_panel_remove(&ctx->panel);
-
-	return 0;
 }
 
 /* ------------------------------------------------------------------------- */
